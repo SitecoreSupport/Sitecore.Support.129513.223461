4714284b51f72e9d7e41ae50823a1165c8df3dd6
DK\mdo <mdo@sitecore.net>
2018-06-04 15:54:45 -0400

    Initial code from patch 129513

---
+++ src/Sitecore.Support.129513.223461/Analytics/Rules/Conditions/GoalWasTriggeredDuringPastOrCurrentInteractionCondition.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Analytics;
+using Sitecore.Analytics.Tracking;
+using Sitecore.Diagnostics;
+using Sitecore.Rules;
+
+namespace Sitecore.Support.Analytics.Rules.Conditions
+{
+  public class GoalWasTriggeredDuringPastOrCurrentInteractionCondition<T> : HasEventOccurredCondition<T>
+    where T : RuleContext
+  {
+    private Guid? goalGuid;
+    private bool goalGuidInitialized;
+
+    public GoalWasTriggeredDuringPastOrCurrentInteractionCondition()
+      : base(false)
+    {
+    }
+
+    protected GoalWasTriggeredDuringPastOrCurrentInteractionCondition(bool filterByCustomData)
+      : base(filterByCustomData)
+    {
+    }
+
+    public string GoalId { get; set; }
+
+    private Guid? GoalGuid
+    {
+      get
+      {
+        if (goalGuidInitialized)
+          return goalGuid;
+        try
+        {
+          goalGuid = new Guid(GoalId);
+        }
+        catch
+        {
+          Log.Warn($"Could not convert value to guid: {GoalId}", GetType());
+        }
+        goalGuidInitialized = true;
+        return goalGuid;
+      }
+    }
+
+    protected override bool Execute(T ruleContext)
+    {
+      Assert.ArgumentNotNull(ruleContext, "ruleContext");
+      Assert.IsNotNull(Tracker.Current, "Tracker.Current is not initialized");
+      Assert.IsNotNull(Tracker.Current.Session, "Tracker.Current.Session is not initialized");
+      Assert.IsNotNull(Tracker.Current.Session.Interaction, "Tracker.Current.Session.Interaction is not initialized");
+      if (!GoalGuid.HasValue)
+        return false;
+      if (HasEventOccurredInInteraction(Tracker.Current.Session.Interaction))
+        return true;
+      Assert.IsNotNull(Tracker.Current.Contact, "Tracker.Current.Contact is not initialized");
+      return FilterKeyBehaviorCacheEntries(Tracker.Current.Contact.GetKeyBehaviorCache()).Any(entry =>
+      {
+        var id = entry.Id;
+        var goalGuid = GoalGuid;
+        if (!goalGuid.HasValue)
+          return false;
+        return id == goalGuid.GetValueOrDefault();
+      });
+    }
+
+    protected override IEnumerable<KeyBehaviorCacheEntry> GetKeyBehaviorCacheEntries(KeyBehaviorCache keyBehaviorCache)
+    {
+      Assert.ArgumentNotNull(keyBehaviorCache, "keyBehaviorCache");
+      return keyBehaviorCache.Goals;
+    }
+
+    protected override bool HasEventOccurredInInteraction(IInteractionData interaction)
+    {
+      Assert.ArgumentNotNull(interaction, "interaction");
+      Assert.IsNotNull(interaction.Pages, "interaction.Pages is not initialized.");
+      return interaction.Pages.SelectMany(page => page.PageEvents).Any(pageEvent =>
+      {
+        if (!pageEvent.IsGoal)
+          return false;
+        var eventDefinitionId = pageEvent.PageEventDefinitionId;
+        var goalGuid = GoalGuid;
+        if (!goalGuid.HasValue)
+          return false;
+        return eventDefinitionId == goalGuid.GetValueOrDefault();
+      });
+    }
+  }
+}
---
+++ src/Sitecore.Support.129513.223461/Analytics/Rules/Conditions/HasEventOccurredCondition.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Analytics.Tracking;
+using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+using ConditionsUtility = Sitecore.Support.Rules.Conditions.ConditionsUtility;
+
+namespace Sitecore.Support.Analytics.Rules.Conditions
+{
+  public abstract class HasEventOccurredCondition<T> : WhenCondition<T> where T : RuleContext
+  {
+    private readonly bool filterByCustomData;
+
+    protected HasEventOccurredCondition(bool filterByCustomData)
+    {
+      this.filterByCustomData = filterByCustomData;
+    }
+
+    public string CustomData { get; set; }
+
+    public string CustomDataOperatorId { get; set; }
+
+    public int NumberOfElapsedDays { get; set; }
+
+    public string NumberOfElapsedDaysOperatorId { get; set; }
+
+    public int NumberOfPastInteractions { get; set; }
+
+    public string NumberOfPastInteractionsOperatorId { get; set; }
+
+    protected virtual IEnumerable<KeyBehaviorCacheEntry> FilterKeyBehaviorCacheEntries(KeyBehaviorCache keyBehaviorCache)
+    {
+      Assert.ArgumentNotNull(keyBehaviorCache, "keyBehaviorCache");
+      var behaviorCacheEntries =
+        FilterKeyBehaviorCacheEntriesByInteractionConditions(
+          keyBehaviorCache.Campaigns.Concat(keyBehaviorCache.Channels)
+            .Concat(keyBehaviorCache.CustomValues)
+            .Concat(keyBehaviorCache.Goals)
+            .Concat(keyBehaviorCache.Outcomes)
+            .Concat(keyBehaviorCache.PageEvents)
+            .Concat(keyBehaviorCache.Venues));
+      if (!filterByCustomData)
+        return
+          Assert.ResultNotNull(GetKeyBehaviorCacheEntries(keyBehaviorCache)
+            .Intersect(behaviorCacheEntries, new KeyBehaviorCacheEntry.KeyBehaviorCacheEntryEqualityComparer()));
+      if (CustomData == null)
+      {
+        Log.Warn("CustomData can not be null", GetType());
+        return Enumerable.Empty<KeyBehaviorCacheEntry>();
+      }
+      behaviorCacheEntries = behaviorCacheEntries.Where(entry =>
+      {
+        return entry.Data != null && ConditionsUtility.CompareStrings(entry.Data, CustomData, CustomDataOperatorId);
+      });
+      return
+        Assert.ResultNotNull(GetKeyBehaviorCacheEntries(keyBehaviorCache)
+          .Intersect(behaviorCacheEntries, new KeyBehaviorCacheEntry.KeyBehaviorCacheEntryEqualityComparer()));
+    }
+
+    protected virtual IEnumerable<KeyBehaviorCacheEntry> FilterKeyBehaviorCacheEntriesByInteractionConditions(
+      IEnumerable<KeyBehaviorCacheEntry> keyBehaviorCacheEntries)
+    {
+      Assert.ArgumentNotNull(keyBehaviorCacheEntries, "keyBehaviorCacheEntries");
+      if (ConditionsUtility.GetInt32Comparer(NumberOfElapsedDaysOperatorId) == null)
+        return Enumerable.Empty<KeyBehaviorCacheEntry>();
+      var numberOfPastInteractionsComparer = ConditionsUtility.GetInt32Comparer(NumberOfPastInteractionsOperatorId);
+      var numberOfElapsedDaysOperatorsComparer = ConditionsUtility.GetInt32Comparer(NumberOfElapsedDaysOperatorId);
+      if (numberOfPastInteractionsComparer == null)
+        return Enumerable.Empty<KeyBehaviorCacheEntry>();
+      return Assert.ResultNotNull(keyBehaviorCacheEntries.GroupBy(entry => new
+      {
+        entry.InteractionId,
+        entry.InteractionStartDateTime
+      }).OrderByDescending(entries => entries.Key.InteractionStartDateTime).Where((entries, i) =>
+      {
+        if (numberOfElapsedDaysOperatorsComparer((DateTime.UtcNow - entries.Key.InteractionStartDateTime).Days,
+          NumberOfElapsedDays))
+          return numberOfPastInteractionsComparer(i + 2, ((HasEventOccurredCondition<T>)this).NumberOfPastInteractions);
+        return false;
+      }).SelectMany(entries => (IEnumerable<KeyBehaviorCacheEntry>)entries));
+    }
+
+    protected abstract IEnumerable<KeyBehaviorCacheEntry> GetKeyBehaviorCacheEntries(KeyBehaviorCache keyBehaviorCache);
+
+    protected abstract bool HasEventOccurredInInteraction(IInteractionData interaction);
+  }
+}
